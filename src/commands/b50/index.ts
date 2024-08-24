632db22d28c475ea7a6da6aa9e91fbbb68fdb0df
saltcute <me@mizuno.ai>
2024-08-24 01:00:33 -0400

    feat: support multiple database

--- src/commands/b50/index.ts
+++ src/commands/b50/index.ts
@@ -8,7 +8,7 @@ import Kasumi, {
 import { DivingFish } from "../../lib/divingFish";
 import { MaiDraw } from "@/lib/maiDraw";
 import { LXNS } from "@/lib/lxns";
-import { IChart } from "@/lib/maiDraw/type";
+import { EDifficulty, IChart } from "@/lib/maiDraw/type";
 
 export default class Best50Command extends BaseCommand<Kasumi<CustomStorage>> {
     name = "b50";
@@ -32,33 +32,98 @@ export default class Best50Command extends BaseCommand<Kasumi<CustomStorage>> {
         });
     }
     func: CommandFunction<BaseSession, any> = async (session) => {
+        console.log(session.args);
         const username = session.args[0];
-        if (!username) return session.reply("请输入用户名");
-        const data = await DivingFish.getPlayerBest50(username);
-        if (!data) return session.reply("获取用户资料失败");
+        const source = session.args[session.args.indexOf("\\-\\-source") + 1];
+        const theme = session.args[session.args.indexOf("\\-\\-theme") + 1];
+
         const { data: msg } = await session.send("正在生成图片...");
-        let chartList: IChart[];
-        if (
-            this.client.config.getSync("maimai::config.useLocalDatabase") &&
-            this.client.config.getSync("maimai::config.localDatabasePath")
-        ) {
-            chartList = [...data.charts.sd, ...data.charts.dx]
-                .map((chart) => {
-                    return this.maiDraw.getLocalChart(
-                        chart.song_id,
-                        chart.level_index
-                    );
-                })
-                .filter((v) => v !== null);
-        } else {
-            chartList = await this.lxns.getSaltChartList();
+        let result: Buffer | null = null;
+        switch (source.toLowerCase()) {
+            case "divingfish":
+            case "df":
+            case "水鱼": {
+                if (!username) return session.reply("请输入用户名");
+                const data = await DivingFish.getPlayerBest50(username);
+                if (!data) return session.reply("获取用户资料失败");
+                let chartList: IChart[];
+                if (
+                    this.client.config.getSync(
+                        "maimai::config.useLocalDatabase"
+                    ) &&
+                    this.client.config.getSync(
+                        "maimai::config.localDatabasePath"
+                    )
+                ) {
+                    chartList = [...data.charts.sd, ...data.charts.dx]
+                        .map((chart) => {
+                            return this.maiDraw.getLocalChart(
+                                chart.song_id,
+                                chart.level_index
+                            );
+                        })
+                        .filter((v) => v !== null);
+                } else {
+                    chartList = await this.lxns.getSaltChartList();
+                }
+                result = await this.maiDraw.draw(
+                    data.nickname,
+                    data.rating,
+                    await DivingFish.toSalt(data.charts.dx, chartList),
+                    await DivingFish.toSalt(data.charts.sd, chartList),
+                    {
+                        scale: 1.5,
+                        theme,
+                    }
+                );
+                break;
+            }
+            case "lxns":
+            case "lx":
+            case "落雪":
+            default: {
+                if (!username) return session.reply("请输入好友代码");
+                const data = (await this.lxns.getPlayerBest50(username))?.data;
+                if (!data) return session.reply("获取用户资料失败");
+                let chartList: IChart[];
+                if (
+                    this.client.config.getSync(
+                        "maimai::config.useLocalDatabase"
+                    ) &&
+                    this.client.config.getSync(
+                        "maimai::config.localDatabasePath"
+                    )
+                ) {
+                    chartList = [...data.dx, ...data.standard]
+                        .map((chart) => {
+                            return this.maiDraw.getLocalChart(
+                                chart.id +
+                                    (chart.type == LXNS.ESongTypes.DX
+                                        ? 10000
+                                        : 0),
+                                chart.level_index as unknown as EDifficulty
+                            );
+                        })
+                        .filter((v) => v !== null);
+                } else {
+                    chartList = await this.lxns.getSaltChartList();
+                }
+                const profile = (await this.lxns.getPlayerProfile(username))
+                    ?.data;
+                result = await this.maiDraw.draw(
+                    profile?.name ?? session.author.nickname,
+                    data.dx_total + data.standard_total,
+                    this.lxns.toSaltScore(data.dx, chartList),
+                    this.lxns.toSaltScore(data.standard, chartList),
+                    {
+                        scale: 1.5,
+                        theme,
+                    }
+                );
+                break;
+            }
         }
-        const result = await this.maiDraw.draw(
-            data.nickname,
-            data.rating,
-            await DivingFish.toSalt(data.charts.dx, chartList),
-            await DivingFish.toSalt(data.charts.sd, chartList)
-        );
+
         if (result) {
             const { data, err } = await this.client.API.asset.create(result);
             if (err) return session.reply("上传图片失败");
--- src/lib/lxns/index.ts
+++ src/lib/lxns/index.ts
@@ -1,6 +1,12 @@
 import axios, { AxiosInstance } from "axios";
 import { Cache } from "memory-cache";
-import { IChart } from "../maiDraw/type";
+import {
+    EAchievementTypes,
+    EComboTypes,
+    ESyncTypes,
+    IChart,
+    IScore,
+} from "../maiDraw/type";
 
 export class LXNS {
     private cache = new Cache<string, object>();
@@ -19,19 +25,36 @@ export class LXNS {
     private async get<T>(
         endpoint: string,
         data?: any
-    ): Promise<LXNS.IAPIResponse<T>> {
-        return (await this.axios.get(endpoint, { params: data })).data;
+    ): Promise<LXNS.IAPIResponse<T> | undefined> {
+        return await this.axios
+            .get(endpoint, { params: data })
+            .then((r) => {
+                console.log(r.data);
+                return r.data;
+            })
+            .catch((e) => {
+                e.response?.data
+                    ? console.log(e.response?.data)
+                    : console.log(e);
+                return e.response?.data;
+            });
     }
     private async post<T>(
         endpoint: string,
         data?: any
-    ): Promise<LXNS.IAPIResponse<T>> {
-        return (await this.axios.post(endpoint, data)).data;
+    ): Promise<LXNS.IAPIResponse<T> | undefined> {
+        return await this.axios
+            .post(endpoint, data)
+            .then((r) => r.data)
+            .catch((e) => e.response?.data);
     }
     async getPlayerBest50(friendCode: string) {
         return await this.get<LXNS.IBest50Response>(
             `/player/${friendCode}/bests`
         );
+    }
+    async getPlayerProfile(friendCode: string) {
+        return await this.get<LXNS.IPlayer>(`/player/${friendCode}`);
     }
     async getSong(songId: number): Promise<LXNS.ISong> {
         const songList = await this.getSongList();
@@ -107,11 +130,92 @@ export class LXNS {
 
         return [...std, ...dx, ...utage];
     }
+    toSaltScore(scores: LXNS.IScore[], chartList: IChart[]): IScore[] {
+        return scores
+            .map((chart) => {
+                const chartDetail = chartList.find(
+                    (v) =>
+                        v.id ===
+                        chart.id +
+                            (chart.type == LXNS.ESongTypes.DX ? 10000 : 0)
+                );
+                if (!chartDetail) return null;
+                return {
+                    chart: chartDetail,
+                    achievement: chart.achievements,
+                    achievementRank: (() => {
+                        switch (chart.rate) {
+                            case LXNS.ERateTypes.C:
+                                return EAchievementTypes.C;
+                            case LXNS.ERateTypes.B:
+                                return EAchievementTypes.B;
+                            case LXNS.ERateTypes.BB:
+                                return EAchievementTypes.BB;
+                            case LXNS.ERateTypes.BBB:
+                                return EAchievementTypes.BBB;
+                            case LXNS.ERateTypes.A:
+                                return EAchievementTypes.A;
+                            case LXNS.ERateTypes.AA:
+                                return EAchievementTypes.AA;
+                            case LXNS.ERateTypes.AAA:
+                                return EAchievementTypes.AAA;
+                            case LXNS.ERateTypes.S:
+                                return EAchievementTypes.S;
+                            case LXNS.ERateTypes.SP:
+                                return EAchievementTypes.SP;
+                            case LXNS.ERateTypes.SS:
+                                return EAchievementTypes.SS;
+                            case LXNS.ERateTypes.SSP:
+                                return EAchievementTypes.SSP;
+                            case LXNS.ERateTypes.SSS:
+                                return EAchievementTypes.SSS;
+                            case LXNS.ERateTypes.SSSP:
+                                return EAchievementTypes.SSSP;
+                            default:
+                                return EAchievementTypes.D;
+                        }
+                    })(),
+                    combo: (() => {
+                        switch (chart.fc) {
+                            case LXNS.EFCTypes.FULL_COMBO:
+                                return EComboTypes.FULL_COMBO;
+                            case LXNS.EFCTypes.FULL_COMBO_PLUS:
+                                return EComboTypes.FULL_COMBO_PLUS;
+                            case LXNS.EFCTypes.ALL_PERFECT:
+                                return EComboTypes.ALL_PERFECT;
+                            case LXNS.EFCTypes.ALL_PERFECT_PLUS:
+                                return EComboTypes.ALL_PERFECT_PLUS;
+                            default:
+                                return EComboTypes.NONE;
+                        }
+                    })(),
+                    sync: (() => {
+                        switch (chart.fs) {
+                            case LXNS.EFSTypes.SYNC:
+                                return ESyncTypes.SYNC_PLAY;
+                            case LXNS.EFSTypes.FULL_SYNC:
+                                return ESyncTypes.FULL_SYNC;
+                            case LXNS.EFSTypes.FULL_SYNC_PLUS:
+                                return ESyncTypes.FULL_SYNC_PLUS;
+                            case LXNS.EFSTypes.FULL_SYNC_DX:
+                                return ESyncTypes.FULL_SYNC_DX;
+                            case LXNS.EFSTypes.FULL_SYNC_DX_PLUS:
+                                return ESyncTypes.FULL_SYNC_DX_PLUS;
+                            default:
+                                return ESyncTypes.NONE;
+                        }
+                    })(),
+                    dxRating: chart.dx_rating,
+                    dxScore: chart.dx_score,
+                };
+            })
+            .filter((v) => v !== null);
+    }
 }
 
 export namespace LXNS {
     /**
-     * Utage charts uses `ILevelIndex.BASIC`,
+     * Utage charts uses `ELevelIndex.BASIC`,
      */
     export enum ELevelIndex {
         BASIC,
@@ -229,7 +333,20 @@ export namespace LXNS {
         left: INotes;
         right: INotes;
     }
-
+    export interface IPlayer {
+        name: string;
+        rating: number;
+        friend_code: number;
+        trophy: any;
+        trophy_name: string;
+        course_rank: string;
+        class_rank: string;
+        star: number;
+        icon: any;
+        name_plate: any;
+        frame: any;
+        upload_time: any;
+    }
     export interface IBest50Response {
         /**
          * Old versions total rating.
